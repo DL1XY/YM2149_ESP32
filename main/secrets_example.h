3fd3a4e676ee3f3b25a824259f20aec2cb073b71
arne woerheide <arnewo@hotmail.com>
2020-04-28 10:27:51 +0200

    secrets_example.h added

---
+++ main/secrets_example.h
@@ -0,0 +1,15 @@
+/*
+ * secrets.h
+ *
+ *  Created on: 28.04.2020
+ *      Author: DL1XY
+ */
+
+#ifndef MAIN_SECRETS_H_
+#define MAIN_SECRETS_H_
+
+#define ESP_WIFI_SSID "YOUR_SSID"
+#define ESP_WIFI_PASS "YOUR_PASSWORD"
+#define MAX_STA_CONN 1 // No one else should be connected
+
+#endif /* MAIN_SECRETS_H_ */
